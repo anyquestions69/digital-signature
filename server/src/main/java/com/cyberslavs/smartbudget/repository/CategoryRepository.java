9cc4612d88d49e5897466f90f0515125f8383e12
public_hysteria <juuzodes@yandex.ru>
2024-07-05 11:55:44 +0300

    backend from scratch

--- server/src/main/java/com/cyberslavs/smartbudget/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cyberslavs.smartbudget.repository;
-
-import com.cyberslavs.smartbudget.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
