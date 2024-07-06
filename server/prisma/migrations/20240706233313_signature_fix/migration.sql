4882a9d5fcc02dc2a131eef5b4f04843a57d5100
public_hysteria <juuzodes@yandex.ru>
2024-07-07 02:33:35 +0300

    signature fix

---
+++ server/prisma/migrations/20240706233313_signature_fix/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `assignedBy` on the `Signature` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Signature" DROP COLUMN "assignedBy";
