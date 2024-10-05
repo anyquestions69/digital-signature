b42f9533ae362aa961f24a5bd015f760c12f0dce
ta1kin <ilataraskin475@gmail.com>
2024-10-05 13:07:29 +0300

    resolve conflict

--- client/src/pages/components/Pdf/PdfContent.vue
+++ client/src/pages/components/Pdf/PdfContent.vue
@@ -1,28 +1,25 @@
 <template>
-    <div class="main__pdf">
-        <div :style="{ width: '1028px', height: '700px' }">
-            <VPdfViewer
-            src="/src/assets/pdf/3.pdf"
-            base="http://localhost:5175"
-            />
-        </div>
-    </div>
+	<div class="main__pdf">
+		<div :style="{ width: '1028px', height: '700px' }">
+			<VPdfViewer src="/src/assets/pdf/3.pdf" base="http://localhost:5173" />
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
-import { VPdfViewer } from "@vue-pdf-viewer/viewer";
+import { VPdfViewer } from '@vue-pdf-viewer/viewer'
 </script>
 
 <style lang="scss">
 .main__pdf {
-    margin: 100px 0;
-    @include Flex;
-    pointer-events: none;
+	margin: 100px 0;
+	@include Flex;
+	pointer-events: none;
 
-    .pdf__content {
-        width: 400px;
-        height: 500px;
-        pointer-events: auto;
-    }
+	.pdf__content {
+		width: 400px;
+		height: 500px;
+		pointer-events: auto;
+	}
 }
 </style>
