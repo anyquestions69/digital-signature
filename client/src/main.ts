252acbfcb1664971e91c22d8100a5ef65db7e159
unknown <Rick-Sanches2020@yandex.ru>
2024-10-11 23:55:25 +0300

    add localStore

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,9 @@
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import './assets/scss/global.scss'
 import router from './routes/router'
 
-createApp(App).use(createPinia()).use(router).mount('#app')
+createApp(App).use( createPinia().use( piniaPluginPersistedstate ) ).use(router).mount('#app')
