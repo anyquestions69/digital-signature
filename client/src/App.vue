029b3d6af11d9d1047da4af69d03f27ec31d820a
ta1kin <ilataraskin475@gmail.com>
2024-10-12 13:58:59 +0300

    toast

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="wrapper">
-    <RouterView />
-  </div>
+	<div class="wrapper">
+		<RouterView />
+	</div>
 </template>
 
 <script setup lang="ts">
-  import { RouterView } from 'vue-router'
+import { onMounted, onUnmounted } from 'vue'
+import { RouterView } from 'vue-router'
+import { useToast } from 'vue-toastification'
+import socket from './socket'
+import { authStore } from './store/authStore'
+
+const toast = useToast()
+const AuthStore = authStore()
+const role = AuthStore.role
+onMounted(() => {
+	if (role === 'Admin') {
+		socket.on('postSigned', (data: { postId: number; message: string }) => {
+			console.log(`Пост ID: ${data.postId} - ${data.message}`)
+			toast.success(`Пост ID: ${data.postId} - ${data.message}`)
+		})
+	}
+})
+
+onUnmounted(() => {
+	socket.off('postSigned')
+})
 </script>
 
 <style lang="scss">
-
-
 .wrapper {
-  width: 100%;
-  overflow: hidden;
+	width: 100%;
+	overflow: hidden;
 }
 </style>
--- client/src/pages/ClientPage.vue
+++ client/src/pages/ClientPage.vue
@@ -8,18 +8,13 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted } from 'vue'
-import socket from '../socket'
-import { authStore } from '../store/authStore'
 import { pagesStore } from '../store/pagesStore'
 import ClientConteiner from './components/Client/ClientConteiner.vue'
 import ClientEditPersonalInfo from './components/Client/ClientEditPersonalInfo.vue'
 import Footer from './components/Footer/Footer.vue'
 import Header from './components/Header/Header.vue'
 
-const AuthStore = authStore()
 const PagesStore = pagesStore()
-const role = AuthStore.role
 
 const headerContent = {
 	button: {
@@ -28,17 +23,4 @@ const headerContent = {
 	},
 	nav: []
 }
-
-onMounted(() => {
-	console.log('Socket')
-	if (role === 'Admin') {
-		socket.on('postSigned', (data: { postId: number; message: string }) => {
-			alert(`Пост ID: ${data.postId} - ${data.message}`)
-		})
-	}
-})
-
-onUnmounted(() => {
-	socket.off('postSigned')
-})
 </script>
