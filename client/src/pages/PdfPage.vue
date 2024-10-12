a9ece8105c6bc7ee55486abe5d21694cd58dc1fd
ta1kin <ilataraskin475@gmail.com>
2024-10-12 10:55:36 +0300

    add sign

--- client/src/pages/PdfPage.vue
+++ client/src/pages/PdfPage.vue
@@ -1,21 +1,21 @@
 <template>
-    <Header :headerContent="headerContent" />
-    <main class="wrapper__main-pdf box">
-        <PdfContent />
-    </main>
-    <Footer />
+	<Header :headerContent="headerContent" />
+	<main class="wrapper__main-pdf box">
+		<PdfContent />
+	</main>
+	<Footer />
 </template>
 
 <script setup lang="ts">
-import PdfContent from "./components/Pdf/PdfContent.vue"
-import Footer from './components/Footer/Footer.vue';
-import Header from './components/Header/Header.vue';
+import Footer from './components/Footer/Footer.vue'
+import Header from './components/Header/Header.vue'
+import PdfContent from './components/Pdf/PdfContent.vue'
 
 const headerContent = {
-    button: {
-        to: '/client/documents',
-        text: 'Список документов'
-    },
-    nav: [] 
+	button: {
+		to: '/client/documents',
+		text: 'Список документов'
+	},
+	nav: []
 }
 </script>
---
+++ client/src/types/pdfjs-dist.d.ts
@@ -0,0 +1,4 @@
+declare module 'pdfjs-dist' {
+	export const GlobalWorkerOptions: any
+	export function getDocument(params: any): any
+}
