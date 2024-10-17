749f949b4cb9d2ccd18c097ccbfe4237c2a4b261
unknown <Rick-Sanches2020@yandex.ru>
2024-10-17 14:15:18 +0300

    исправил ошибку с всплывающим окном

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ dotenv.config()
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	//app.setGlobalPrefix('api')
+	app.setGlobalPrefix('api')
 	// Настройка CORS
 	const corsOptions = {
 		origin: process.env.CORS_ORIGIN || '*',
